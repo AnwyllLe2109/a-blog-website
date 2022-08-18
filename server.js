35344e7c3f27d299c20a1cf9574fbd51d31079c4
Anwyll Le <lelinhnguyen2109@gmail.com>
2022-08-18 01:26:52 -0400

    18/08/2022 - 1:26AM

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const express = require("express");
 const app = express();
 //set up port
 let PORT = process.env.PORT || 8080;
+//set up handlebars
+const exphbs = require("express-handlebars")
+app.engine('.hbs', exphbs.engine({ 
+    extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
+
+//use this line to connect the app to bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 
 app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/layouts/main.html"));
+    // res.sendFile(path.join(__dirname, "/views/layouts/main.html"));
+    res.render("index");
 })
 
 app.listen(PORT, ()=>{
